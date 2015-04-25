47bf2424e88321b0ab946b5c82f63143689f184b
Andrew Reynolds <andrew@chambana.net>
2015-04-25 14:55:19 -0500

    1.15: fix intentional bugs to resolve project build errors

--- Chapter 1/1.15.cpp
+++ Chapter 1/1.15.cpp
@@ -7,27 +7,27 @@
 
 /* Syntax errors */
 // Missing ) in the parameter list for main
-int main(
+int main()
 {
-    // used colon, not a semicolon, after endl
-    std::cout << "Read each file." << std::endl:
+    // used colon, not a semicolon, at end of line
+    std::cout << "Read each file." << std::endl;
 
     // missing quotes around string literal
-    std::cout << Update master. << std::endl;
+    std::cout << "Update master." << std::endl;
 
     // second output operator is missing
-    std::cout << "Write new master" std::endl;
+    std::cout << "Write new master" << std::endl;
 
     /* Type errors */
-    int typerror = "Hello"
+    std::string typerror = "Hello";
 
     /* Declaration Errors */
     int v1 = 0, v2 = 0;
     // Uses v, not v1
-    std::cin >> v >> v2;
+    std::cin >> v1 >> v2;
 
     // cout not defined. Should be std::cout
-    cout << v1 + v2 << std::endl;
+    std::cout << v1 + v2 << std::endl;
 
     return 0;
 }
